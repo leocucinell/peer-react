270a806f060b4053975f31c272f26fba1ac505bd
Leo Cucinell <leocucinell@gmail.com>
2022-04-11 17:23:18 -1000

    added react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
